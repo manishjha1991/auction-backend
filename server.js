ec18946ba7b8d6eee73bdfb9be4804573ef1d737
Manish <mkjha9555@gmail.com>
2025-08-20 14:47:15 +0530

    revert for trail

--- server.js
+++ server.js
@@ -21,7 +21,7 @@ const io = new Server(server, { cors: { origin: '*' } });
 const tradeRoutes = require('./routes/trades');
 const liveScoreRoutes = require('./routes/livescores');
 app.set('io', io);
-mongoose.connect(process.env.MONGO_URI, { dbName: 'cpl_11',useNewUrlParser: true, useUnifiedTopology: true })
+mongoose.connect(process.env.MONGO_URI, { dbName: 'cpl_12',useNewUrlParser: true, useUnifiedTopology: true })
   .then(() => console.log('MongoDB Connected'))
   .catch(err => console.error(err));
 app.use('/uploads', express.static('uploads'));
