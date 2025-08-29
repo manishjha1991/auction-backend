24244408510f648a172e1cf57b5324054c553ff9
Manish <mkjha9555@gmail.com>
2025-08-29 23:17:17 +0530

    feed

--- server.js
+++ server.js
@@ -28,7 +28,7 @@ const commentRoutes = require('./routes/comments');
 const postLikeRoutes = require('./routes/postLikes');
 
 app.set('io', io);
-mongoose.connect(process.env.MONGO_URI, { dbName: 'cpl_12',useNewUrlParser: true, useUnifiedTopology: true })
+mongoose.connect(process.env.MONGO_URI, { dbName: 'cpl_13',useNewUrlParser: true, useUnifiedTopology: true })
   .then(() => console.log('MongoDB Connected'))
   .catch(err => console.error(err));
 app.use('/uploads', express.static('uploads'));
