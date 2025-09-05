d9848f4eb9cf2e2e124c7adb2add48499adadb42
Manish <mkjha9555@gmail.com>
2025-09-05 10:14:40 +0530

    add cpl_12

--- server.js
+++ server.js
@@ -30,7 +30,7 @@ const playoffFixtureRoutes = require('./routes/playoffFixtures');
 const scheduleRoutes = require('./routes/schedules');
 
 app.set('io', io);
-mongoose.connect(process.env.MONGO_URI, { dbName: 'cpl_13',useNewUrlParser: true, useUnifiedTopology: true })
+mongoose.connect(process.env.MONGO_URI, { dbName: 'cpl_12',useNewUrlParser: true, useUnifiedTopology: true })
   .then(() => console.log('MongoDB Connected'))
   .catch(err => console.error(err));
 app.use('/uploads', express.static('uploads'));
