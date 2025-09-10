4f1a2c56528563ddf4e24dd8833da6dc09914fcd
Manish <mkjha9555@gmail.com>
2025-09-10 22:19:32 +0530

    add schedular

--- server.js
+++ server.js
@@ -35,7 +35,7 @@ const retainedPlayerRoutes = require('./routes/retainedPlayers');
 app.set('io', io);
 // 🚀 OPTIMIZED MongoDB Connection Pooling Configuration
 const mongooseOptions = {
-  dbName: 'cpl_12',
+  dbName: 'cpl_12_auction_ready',
   useNewUrlParser: true,
   useUnifiedTopology: true,
   
