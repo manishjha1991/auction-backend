7a44b8f931a80268653e58ffc38901be4afaaf2f
Manish <mkjha9555@gmail.com>
2025-11-28 23:52:20 +0530

    add alloow device fingertip

--- scripts/fixAuctionState.js
+++ scripts/fixAuctionState.js
@@ -54,7 +54,7 @@ function printIssues(preview) {
 }
 
 async function run() {
-  await mongoose.connect(uri, { serverSelectionTimeoutMS: 5000, dbName: 'cpl_14' });
+  await mongoose.connect(uri, { serverSelectionTimeoutMS: 5000, dbName: 'cpl_17' });
   console.log('Connected to Mongo');
 
   const preview = await previewAuctionFixes();
