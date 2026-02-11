97f8c9d3f83c209f6e7825605198231cfb5c25dd
Manish <mkjha9555@gmail.com>
2026-02-11 18:03:04 +0530

    add fixes

--- server.js
+++ server.js
@@ -35,7 +35,7 @@ const tournamentRoutes = require('./routes/tournaments');
 const matchResultsRoutes = require('./routes/matchResults');
 const adminToolsRoutes = require('./routes/adminTools');
 const monitoringRoutes = require('./routes/monitoring');
-// require('./scheduler');
+require('./scheduler');
 
 // Allow Express to trust proxy headers (needed to read real client IPs)
 app.set('trust proxy', true);
